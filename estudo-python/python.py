103463ffc76172867cd910051ee16951629663f5
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-01-13 20:03:00 -0300

    my start in : python

---
+++ estudo-python/python.py
@@ -0,0 +1,5 @@
+# passo 1 abrir o sistema da empresa
+# passo 2 fazer login
+# passo 3 importar base de dados dos produtos
+# passo 4 cadastrar um produto 
+# passo 5 repetir o passo 4 atr acabar tudo
