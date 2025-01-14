f22043152cef57bbf019ad4a2234330efa1d9cb6
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-01-14 10:24:31 -0300

    .

--- estudo-python/python.py
+++ estudo-python/python.py
@@ -1,4 +1,5 @@
 # passo 1 abrir o sistema da empresa
+# Link sistema teste: https://dlp.hashtagtreinamentos.com/python/intensivao/login
 # passo 2 fazer login
 # passo 3 importar base de dados dos produtos
 # passo 4 cadastrar um produto 
